977b89945d554428be7f965019a08d5a961d223a
NeoNatural <42388348+NeoNatural@users.noreply.github.com>
2025-12-12 11:45:44 +0000

    20251210

--- examples/PVGG16/generate_pvgg16.py
+++ examples/PVGG16/generate_pvgg16.py
@@ -2,4 +2,4 @@ from predify import predify
 from torchvision.models import VGG16_BN_Weights, vgg16_bn
 
 weights = VGG16_BN_Weights.IMAGENET1K_V1
-predify(vgg16_bn(weights=weights), './pvgg16_config.toml')
+predify(vgg16_bn(weights=weights), './pvgg16_config.toml','./try_gen.py')
